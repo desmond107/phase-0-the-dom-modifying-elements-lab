a82b33afa66187e0a3cac865f2b8ff70b7d9e0ab
desmond <desmond@desmonds-MacBook-Pro.local>
2022-04-15 10:23:52 +0300

    task complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute('id',"victory")
+
+newHeader.innerHTML = "<h1>Desmond is the champion</h1>"
